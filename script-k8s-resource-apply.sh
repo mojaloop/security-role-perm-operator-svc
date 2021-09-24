b5d88839d3d3ba7332d995e1c08979d03dafe848
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-09-24 19:06:11 +0530

    Added keto serivce k8s script and fixed the deployment scripts

---
+++ script-k8s-resource-apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAMESPACE=moja-operator
+OPERATION=$1
+
+kubectl apply -f resources/moja-operator-namespace.yaml
+kubectl apply -f resources/mojaloop-namespace.yaml
+kubectl -n $NAMESPACE apply -f resources/moja-role-operator-sa.yaml
+kubectl -n $NAMESPACE apply -f resources/mojalooprole-crd.yaml
+kubectl -n $NAMESPACE apply -f resources/mojalooprole-editor-role.yaml
+kubectl -n $NAMESPACE apply -f resources/moja-role-operator-clusterrolebinding.yaml
+kubectl -n $NAMESPACE apply -f resources/moja-role-operator-deployment.yaml
+kubectl -n $NAMESPACE apply -f resources/moja-keto.yaml
---
+++ script-k8s-resource-delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NAMESPACE=moja-operator
+OPERATION=$1
+
+kubectl -n $NAMESPACE delete -f resources/moja-role-operator-deployment.yaml
+kubectl -n $NAMESPACE delete -f resources/moja-role-operator-clusterrolebinding.yaml
+kubectl -n $NAMESPACE delete -f resources/mojalooprole-editor-role.yaml
+kubectl -n $NAMESPACE delete -f resources/mojalooprole-crd.yaml
+kubectl -n $NAMESPACE delete -f resources/moja-role-operator-sa.yaml
+kubectl -n $NAMESPACE delete -f resources/moja-keto.yaml
+kubectl delete -f resources/mojaloop-namespace.yaml
+kubectl delete -f resources/moja-operator-namespace.yaml
+
