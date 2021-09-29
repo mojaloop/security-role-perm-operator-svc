3e9ef3e96a529f7d53026795fb84aa81d868c77d
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-09-29 21:53:46 +0530

    Added k8s steps in ci

--- script-k8s-resource-apply.sh
+++ script-k8s-resource-apply.sh
@@ -10,3 +10,5 @@ kubectl -n $NAMESPACE apply -f resources/mojalooprole-editor-role.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-role-operator-clusterrolebinding.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-role-operator-deployment.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-keto.yaml
+kill -9 $(pgrep -f "minikube tunnel")
+minikube tunnel -c &> /dev/null &
