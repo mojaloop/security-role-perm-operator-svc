d0ffcd5233c58122f50e3c53d232f1f5d2f1b724
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-11-26 00:57:12 +0530

    Fixed integration tests

--- script-k8s-resource-apply.sh
+++ script-k8s-resource-apply.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-NAMESPACE=moja-operator
+NAMESPACE=mojaloop
 OPERATION=$1
 
-kubectl apply -f resources/moja-operator-namespace.yaml
 kubectl apply -f resources/mojaloop-namespace.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-role-operator-sa.yaml
 kubectl -n $NAMESPACE apply -f resources/mojalooprole-crd.yaml
 kubectl -n $NAMESPACE apply -f resources/mojaloop-permission-exclusions-crd.yaml
 kubectl -n $NAMESPACE apply -f resources/mojalooprole-editor-role.yaml
-kubectl -n $NAMESPACE apply -f resources/moja-role-operator-clusterrolebinding.yaml
+kubectl -n $NAMESPACE apply -f resources/moja-role-operator-rolebinding.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-role-operator-deployment.yaml
 kubectl -n $NAMESPACE apply -f resources/moja-keto.yaml
--- script-k8s-resource-delete.sh
+++ script-k8s-resource-delete.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-NAMESPACE=moja-operator
+NAMESPACE=mojaloop
 OPERATION=$1
 kubectl -n $NAMESPACE delete -f resources/moja-role-operator-deployment.yaml
-kubectl -n $NAMESPACE delete -f resources/moja-role-operator-clusterrolebinding.yaml
+kubectl -n $NAMESPACE delete -f resources/moja-role-operator-rolebinding.yaml
 kubectl -n $NAMESPACE delete -f resources/mojalooprole-editor-role.yaml
 kubectl -n $NAMESPACE delete -f resources/mojaloop-permission-exclusions-crd.yaml
 kubectl -n $NAMESPACE delete -f resources/mojalooprole-crd.yaml
 kubectl -n $NAMESPACE delete -f resources/moja-role-operator-sa.yaml
 kubectl -n $NAMESPACE delete -f resources/moja-keto.yaml
 kubectl delete -f resources/mojaloop-namespace.yaml
-kubectl delete -f resources/moja-operator-namespace.yaml
 
