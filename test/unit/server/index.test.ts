65ca16e819bd1e79f95b437fa2c25424721655e8
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-12-15 20:48:15 +0530

    Added few unit tests

---
+++ test/unit/server/index.test.ts
@@ -0,0 +1,69 @@
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the
+ Apache License, Version 2.0 (the 'License') and you may not use these files
+ except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files
+ are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Vijay Kumar Guthi <vijaya.guthi@modusbox.com>
+
+ --------------
+ ******/
+/* eslint-disable import/first */
+
+// Mock functions for jest. Keep these functions at the top because jest.mock calls will be hoisted to below these lines
+const mockStartServer = jest.fn()
+
+import Config from '../../../src/shared/config'
+import ServiceServer from '../../../src/server'
+
+jest.mock('@hapi/hapi', () => {
+  return {
+    Server: jest.fn().mockImplementation(() => {
+      return {
+        start: mockStartServer,
+        register: jest.fn(),
+        route: jest.fn(),
+        ext: jest.fn(),
+        info: {
+          uri: 'SOME_URI'
+        }
+      }
+    })
+  }
+})
+
+describe('start', () => {
+  let spyStartServer: jest.Mock
+  beforeAll(() => {
+    spyStartServer = mockStartServer
+  })
+  afterEach(() => {
+    spyStartServer.mockClear()
+  })
+  it('should call server.start method', async () => {
+    const result = await ServiceServer.run(Config)
+    expect(spyStartServer).toHaveBeenCalledTimes(1)
+    expect(result).toHaveProperty('info')
+    expect(result.info).toHaveProperty('uri')
+    expect(result.info.uri).toEqual('SOME_URI')
+  })
+})
---
+++ test/unit/server/plugins/state.test.ts
@@ -0,0 +1,73 @@
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the
+ Apache License, Version 2.0 (the 'License') and you may not use these files
+ except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files
+ are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Vijay Kumar <vijaya.guthi@modusbox.com>
+
+ --------------
+ ******/
+
+import { StatePlugin } from '~/server/plugins/state'
+import { Server } from '@hapi/hapi'
+import { mockProcessExit } from 'jest-mock-process'
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('~/shared/logger')
+
+describe('StatePlugin', () => {
+  const ServerMock = {
+    events: {
+      on: jest.fn()
+    },
+    decorate: jest.fn()
+  }
+
+  it('should have proper layout', () => {
+    expect(typeof StatePlugin.name).toEqual('string')
+    expect(typeof StatePlugin.version).toEqual('string')
+    expect(StatePlugin.once).toBeTruthy()
+    expect(StatePlugin.register).toBeInstanceOf(Function)
+  })
+
+  it('happy flow: should properly register', async () => {
+    await StatePlugin.register(ServerMock as unknown as Server)
+
+    // check decoration
+    expect(ServerMock.decorate)
+    expect(ServerMock.decorate.mock.calls[0][0]).toEqual('toolkit')
+    expect(ServerMock.decorate.mock.calls[0][1]).toEqual('getLogger')
+  })
+
+  it('exceptions: should properly register', async () => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    mocked(ServerMock.decorate).mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const mockExit = mockProcessExit()
+    await StatePlugin.register(ServerMock as unknown as Server)
+    expect(mockExit).toBeCalledWith(1)
+    mockExit.mockRestore()
+  })
+})
