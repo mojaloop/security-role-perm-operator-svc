ab6980f9977a86950ec490da2df30cb019cf7655
geka.evk <geka.evk@gmail.com>
2024-02-14 09:10:24 +0000

    fix:iprod-388 cleaning up

--- test/integration/permission-exclusions-validation/role-permissions-mapping.test.ts
+++ test/integration/permission-exclusions-validation/role-permissions-mapping.test.ts
@@ -32,7 +32,7 @@ import * as k8s from '@kubernetes/client-node'
 import Config from './config'
 import ServiceConfig from './../../../src/shared/config'
 // eslint-disable-next-line max-len
-import { PermissionExclusionsValidator, RolePermissions, PermissionExclusions } from './../../../src/validation/permission-exclusions'
+import { RolePermissions, PermissionExclusions } from './../../../src/validation/permission-exclusions'
 import role1Resource from './data/cr-role1.json'
 import role2Resource from './data/cr-role2.json'
 import role3Resource from './data/cr-role3.json'
@@ -41,7 +41,7 @@ import pe2Resource from './data/cr-permission-exclusion2.json'
 
 import axios from 'axios'
 
-jest.setTimeout(50000)
+jest.setTimeout(50_000)
 
 ServiceConfig.ORY_KETO_READ_SERVICE_URL = Config.ORY_KETO_READ_SERVICE_URL
 
@@ -52,12 +52,6 @@ kc.loadFromDefault()
 // const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
 const k8sApiCustomObjects = kc.makeApiClient(k8s.CustomObjectsApi)
 // const k8sApiPods = kc.makeApiClient(k8s.CoreV1Api)
-
-const permissionExclusionsValidator = new PermissionExclusionsValidator(ServiceConfig)
-
-const _relationTuplesToPermissionCombos = (relationTuples: Array<any>) => {
-  return relationTuples?.map(item => item.object + ':' + item.subject.replace(/permission:([^#.]*)(#.*)?/, '$1'))
-}
 
 const rolePermissions: RolePermissions[] = [
   {
@@ -86,6 +80,8 @@ const permissionExclusions: PermissionExclusions[] = [
   }
 ]
 
+const waitChanges = () => new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
+
 const cleanup = async () => {
   // Remove user role assignments
   try {
@@ -96,65 +92,35 @@ const cleanup = async () => {
     await axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)
   } catch (err) {}
 
-  // Remove role permission mappings
-  try {
-    await k8sApiCustomObjects.deleteNamespacedCustomObject(
-      Config.WATCH_RESOURCE_GROUP,
-      Config.WATCH_RESOURCE_VERSION,
-      Config.WATCH_NAMESPACE,
-      Config.WATCH_ROLE_PERMISSIONS_RESOURCE_PLURAL,
-      role1Resource.metadata.name
-    )
-  } catch (err) {}
-  try {
-    await k8sApiCustomObjects.deleteNamespacedCustomObject(
-      Config.WATCH_RESOURCE_GROUP,
-      Config.WATCH_RESOURCE_VERSION,
-      Config.WATCH_NAMESPACE,
-      Config.WATCH_ROLE_PERMISSIONS_RESOURCE_PLURAL,
-      role2Resource.metadata.name
-    )
-  } catch (err) {}
-  try {
-    await k8sApiCustomObjects.deleteNamespacedCustomObject(
-      Config.WATCH_RESOURCE_GROUP,
-      Config.WATCH_RESOURCE_VERSION,
-      Config.WATCH_NAMESPACE,
-      Config.WATCH_ROLE_PERMISSIONS_RESOURCE_PLURAL,
-      role3Resource.metadata.name
-    )
-  } catch (err) {}
-  // Remove permission exclusions
-  try {
-    await k8sApiCustomObjects.deleteNamespacedCustomObject(
-      Config.WATCH_RESOURCE_GROUP,
-      Config.WATCH_RESOURCE_VERSION,
-      Config.WATCH_NAMESPACE,
-      Config.WATCH_PERMISSION_EXCLUSIONS_RESOURCE_PLURAL,
-      pe1Resource.metadata.name
-    )
-  } catch (err) {}
-  try {
-    await k8sApiCustomObjects.deleteNamespacedCustomObject(
-      Config.WATCH_RESOURCE_GROUP,
-      Config.WATCH_RESOURCE_VERSION,
-      Config.WATCH_NAMESPACE,
-      Config.WATCH_PERMISSION_EXCLUSIONS_RESOURCE_PLURAL,
-      pe2Resource.metadata.name
-    )
-  } catch (err) {}
+  const cleaning = [
+    // Remove role permission mappings
+    role1Resource,
+    role2Resource,
+    role3Resource,
+    // Remove permission exclusions
+    pe1Resource,
+    pe2Resource
+  ].map(crd => k8sApiCustomObjects.deleteNamespacedCustomObject(
+    Config.WATCH_RESOURCE_GROUP,
+    Config.WATCH_RESOURCE_VERSION,
+    Config.WATCH_NAMESPACE,
+    Config.WATCH_ROLE_PERMISSIONS_RESOURCE_PLURAL,
+    crd.metadata.name
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  ).catch(() => {}))
+
+  await Promise.all(cleaning)
 }
 
 describe('Permission Exclusion Validator', (): void => {
   beforeAll(async () => {
     await cleanup()
+    await waitChanges()
   })
   afterAll(async () => {
     await cleanup()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, 3 * Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Add a role1 permission mapping', async () => {
     const status = await k8sApiCustomObjects.createNamespacedCustomObject(
       Config.WATCH_RESOURCE_GROUP,
@@ -165,6 +131,7 @@ describe('Permission Exclusion Validator', (): void => {
     )
     expect(status.response.statusCode).toEqual(201)
   })
+
   it('Add a role2 permission mapping', async () => {
     const status = await k8sApiCustomObjects.createNamespacedCustomObject(
       Config.WATCH_RESOURCE_GROUP,
@@ -174,16 +141,16 @@ describe('Permission Exclusion Validator', (): void => {
       role2Resource
     )
     expect(status.response.statusCode).toEqual(201)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Validate role permissions', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .resolves.toBeTruthy()
   })
   it('Validate user role assignment prior to actual assignment', async () => {
     const postData = {
@@ -201,14 +168,16 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE1'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData))
+      .resolves.toBeTruthy()
   })
   it('role permissions validation should be passed', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .resolves.toBeTruthy()
   })
   it('Assign role1 and role2 to user1', async () => {
     const postData = {
@@ -218,20 +187,16 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE2'
       ]
     }
-    try {
-      const result = await axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)
-      expect(result).toBeTruthy()
-    } catch (err) {
-      if (err instanceof Error) console.error(`Error in Assign role1 and role2 to user1: ${err?.message}`, err)
-      throw err
-    }
+    const result = await axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)
+    expect(result).toBeTruthy()
   })
   it('rolepermissions validation should be failed', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).rejects.toThrowError()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .rejects.toThrowError()
   })
   it('Un-assign role2 from user1', async () => {
     const postData = {
@@ -240,14 +205,16 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE1'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData))
+      .resolves.toBeTruthy()
   })
   it('rolepermissions validation should be passed', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .resolves.toBeTruthy()
   })
   it('Add a permission exclusion', async () => {
     const status = await k8sApiCustomObjects.createNamespacedCustomObject(
@@ -258,10 +225,9 @@ describe('Permission Exclusion Validator', (): void => {
       pe1Resource
     )
     expect(status.response.statusCode).toEqual(201)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Validate user role assignment prior to actual assignment', async () => {
     const postData = {
       username: 'user1',
@@ -270,7 +236,8 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE2'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/user-role', postData)).rejects.toThrowError()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/user-role', postData))
+      .rejects.toThrowError()
   })
   it('Try to assign role1 and role2 to user1', async () => {
     const postData = {
@@ -280,8 +247,10 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE2'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)).rejects.toThrowError()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData))
+      .rejects.toThrowError()
   })
+
   it('Delete the permission exclusion', async () => {
     const status = await k8sApiCustomObjects.deleteNamespacedCustomObject(
       Config.WATCH_RESOURCE_GROUP,
@@ -291,10 +260,9 @@ describe('Permission Exclusion Validator', (): void => {
       pe1Resource.metadata.name
     )
     expect(status.response.statusCode).toEqual(200)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Assign role1 and role2 to user1 now', async () => {
     const postData = {
       username: 'user1',
@@ -303,16 +271,19 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE2'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData))
+      .resolves.toBeTruthy()
   })
   it('rolepermissions validation should be failed again now', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).rejects.toThrowError()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .rejects.toThrowError()
   })
-  it('If we try to add permission exclusions even after validation failure, the CR should be set with invalid status', async () => {
+
+  it('If we try to add permission exclusions after validation failure, the CR should be set with invalid status', async () => {
     const status = await k8sApiCustomObjects.createNamespacedCustomObject(
       Config.WATCH_RESOURCE_GROUP,
       Config.WATCH_RESOURCE_VERSION,
@@ -321,10 +292,9 @@ describe('Permission Exclusion Validator', (): void => {
       pe1Resource
     )
     expect(status.response.statusCode).toEqual(201)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Status of the custom resource should be set to INVALID', async () => {
     const status = await k8sApiCustomObjects.getNamespacedCustomObjectStatus(
       Config.WATCH_RESOURCE_GROUP,
@@ -347,10 +317,9 @@ describe('Permission Exclusion Validator', (): void => {
       pe1Resource.metadata.name
     )
     expect(status.response.statusCode).toEqual(200)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Un-assign role2 from user1', async () => {
     const postData = {
       username: 'user1',
@@ -358,14 +327,16 @@ describe('Permission Exclusion Validator', (): void => {
         'PEVALROLE1'
       ]
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/assignment/user-role', postData))
+      .resolves.toBeTruthy()
   })
   it('rolepermissions validation should be passed now', async () => {
     const postData = {
       rolePermissions,
       permissionExclusions
     }
-    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData)).resolves.toBeTruthy()
+    await expect(axios.post(Config.PERMISSION_OPERATOR_API_URL + '/validate/role-permissions', postData))
+      .resolves.toBeTruthy()
   })
   it('Add permission exclusions now and check the CR status', async () => {
     const status = await k8sApiCustomObjects.createNamespacedCustomObject(
@@ -376,10 +347,9 @@ describe('Permission Exclusion Validator', (): void => {
       pe1Resource
     )
     expect(status.response.statusCode).toEqual(201)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Status of the custom resource should be set to VALIDATED', async () => {
     const status = await k8sApiCustomObjects.getNamespacedCustomObjectStatus(
       Config.WATCH_RESOURCE_GROUP,
@@ -402,10 +372,9 @@ describe('Permission Exclusion Validator', (): void => {
       role3Resource
     )
     expect(status.response.statusCode).toEqual(201)
+    await waitChanges()
   })
-  it('Wait for some time', async () => {
-    await new Promise(resolve => setTimeout(resolve, Config.WAIT_TIME_MS_AFTER_K8S_RESOURCE_CHANGE))
-  })
+
   it('Status of the custom resource should be set to INVALID', async () => {
     const status = await k8sApiCustomObjects.getNamespacedCustomObjectStatus(
       Config.WATCH_RESOURCE_GROUP,
