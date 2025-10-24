a14fbdfe5a521be36ba206d1b6436c4d603f6fa6
geka.evk <geka.evk@gmail.com>
2025-10-24 13:57:25 +0100

    fix(csi-1885): used default axios

--- src/shared/keto.ts
+++ src/shared/keto.ts
@@ -34,35 +34,38 @@ const log = logger.child({ component: 'KetoRelationshipApiClient' })
 
 const HTTP_TIMEOUT_MS = 10_000 // make configurable
 
-const axiosInstance = axios.create({
-  timeout: HTTP_TIMEOUT_MS,
-  // transitional: {
-  //   // throw ETIMEDOUT error instead of generic ECONNABORTED on request timeouts
-  //   clarifyTimeoutError: false,
-  // },
-})
+axios.defaults.timeout = HTTP_TIMEOUT_MS
+
+
+// const axiosInstance = axios.create({
+//   timeout: HTTP_TIMEOUT_MS,
+//   // transitional: {
+//   //   // throw ETIMEDOUT error instead of generic ECONNABORTED on request timeouts
+//   //   clarifyTimeoutError: false,
+//   // },
+// })
 // todo: add retry logic
 
-axiosInstance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    log.debug('HTTP request succeeded: ', {
-      method: response.config.method?.toUpperCase(),
-      url: response.config.url,
-      status: response.status,
-    })
-    return response
-  },
-  (error: AxiosError) => {
-    log.warn('HTTP request failed: ', {
-      method: error.config?.method?.toUpperCase(),
-      url: error.config?.url,
-      code: error.code,
-      message: error.message,
-      status: error.response?.status,
-    })
-    return Promise.reject(error)
-  }
-)
+// axios.interceptors.response.use(
+//   (response: AxiosResponse) => {
+//     log.debug('HTTP request succeeded: ', {
+//       method: response.config.method?.toUpperCase(),
+//       url: response.config.url,
+//       status: response.status,
+//     })
+//     return response
+//   },
+//   (error: AxiosError) => {
+//     log.warn('HTTP request failed: ', {
+//       method: error.config?.method?.toUpperCase(),
+//       url: error.config?.url,
+//       code: error.code,
+//       message: error.message,
+//       status: error.response?.status,
+//     })
+//     return Promise.reject(error)
+//   }
+// )
 
 export const createKetoRelationshipApiClient = (baseUrl: string) =>
-  new RelationshipApi(undefined, baseUrl, axiosInstance)
+  new RelationshipApi(undefined, baseUrl/*, axiosInstance*/)
